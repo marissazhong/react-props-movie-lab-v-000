e3b00765adb798a973a01be4d09df675d46b8358
Marissa Zhong <mwz2101@columbia.edu>
2018-08-04 15:02:46 +0000

    automatically backed up by learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -11,7 +11,11 @@ const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 export default class CardBack extends Component {
 
   generateRatingElement = () => {
-    // implement meeeee! See the readme for instructions
+    if (this.props.IMDBRating !== null) {
+      
+    } else {
+      <h4>No Rating Found</h4>
+    }
   }
 
   render() {
